c5207826bcb7a6a45e9d57016dbcb3d2ff6250b7
Anh Khoa <dnanhkhoa@live.com>
2018-08-07 02:47:15 +0700

    Add item class

--- vnnewscrawler/vnnewscrawler/items.py
+++ vnnewscrawler/vnnewscrawler/items.py
@@ -5,10 +5,14 @@
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Item, Field
 
 
-class VnnewscrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class Article(Item):
+    url = Field()
+    source = Field()
+    category = Field()
+    title = Field()
+    description = Field()
+    content = Field()
+    time = Field()
