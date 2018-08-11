b55d619df32bd250c2d2560e9852c7c3eee75d9b
Anh Khoa <dnanhkhoa@live.com>
2018-08-11 21:58:59 +0700

    Update Item class

--- vnnewscrawler/vnnewscrawler/items.py
+++ vnnewscrawler/vnnewscrawler/items.py
@@ -6,13 +6,19 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 from scrapy import Item, Field
+from scrapy.contrib.loader import ItemLoader
 
 
 class Article(Item):
+    code = Field()
     url = Field()
     source = Field()
     category = Field()
-    title = Field()
+    headline = Field()
     description = Field()
     content = Field()
-    time = Field()
+    time = Field(serializer=str)
+
+
+class ArticleLoader(ItemLoader):
+    default_item_class = Article
