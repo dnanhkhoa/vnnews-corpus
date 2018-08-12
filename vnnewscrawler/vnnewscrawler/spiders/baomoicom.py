bf0370dc793c11216f2d0dd53fa394537325a1fb
Anh Khoa <dnanhkhoa@live.com>
2018-08-13 00:09:18 +0700

    Fix minor bugs in spider

--- vnnewscrawler/vnnewscrawler/spiders/baomoicom.py
+++ vnnewscrawler/vnnewscrawler/spiders/baomoicom.py
@@ -12,6 +12,7 @@ class BaomoicomSpider(Spider):
     name = "baomoicom"
     home_url = "https://baomoi.com"
     allowed_domains = ["baomoi.com"]
+    disallowed_urls = ["baomoi.com/404"]
 
     linebreak_regex = regex.compile(r"(?:\r*[\n\v])+")
     article_code_regex = regex.compile(r"\/c\/(\d+)\.epi", flags=regex.IGNORECASE)
@@ -55,32 +56,35 @@ class BaomoicomSpider(Spider):
             )
 
     def parse_article(self, response):
-        article_loader = ArticleLoader(response=response)
-        article_loader.add_value(
-            "code", self.article_code_regex.search(response.url).group(1)
-        )
-        article_loader.add_value("url", response.url)
-        article_loader.add_css("category", "meta[property*=section]::attr(content)")
-
-        article_info = json.loads(
-            response.css("script[type='application/ld+json']::text").extract_first(
-                default="{}"
+        if all(map(lambda url: url not in response.url, self.disallowed_urls)):
+            article_loader = ArticleLoader(response=response)
+            article_loader.add_value(
+                "code", self.article_code_regex.search(response.url).group(1)
             )
-        )
-        article_loader.add_value("source", article_info, SelectJmes("author.name"))
-        article_loader.add_value("headline", article_info, SelectJmes("headline"))
-        article_loader.add_value("description", article_info, SelectJmes("description"))
-        article_loader.add_value("time", article_info, SelectJmes("dateModified"))
+            article_loader.add_value("url", response.url)
+            article_loader.add_css("category", "meta[property*=section]::attr(content)")
 
-        article_soup = BeautifulSoup(
-            response.css(".article__body").extract_first(), "lxml"
-        )
-        for br_tag in article_soup.find_all("br"):
-            br_tag.replace_with("\n")
+            article_info = json.loads(
+                response.css("script[type='application/ld+json']::text").extract_first(
+                    default="{}"
+                )
+            )
+            article_loader.add_value("source", article_info, SelectJmes("author.name"))
+            article_loader.add_value("headline", article_info, SelectJmes("headline"))
+            article_loader.add_value(
+                "description", article_info, SelectJmes("description")
+            )
+            article_loader.add_value("time", article_info, SelectJmes("dateModified"))
 
-        lines = []
-        for paragraph_tag in article_soup.find_all("p", class_="body-text"):
-            for line in self.linebreak_regex.split(paragraph_tag.get_text()):
-                lines.append(line.strip())
-        article_loader.add_value("content", "\n".join(lines))
-        yield article_loader.load_item()
+            article_soup = BeautifulSoup(
+                response.css(".article__body").extract_first(), "lxml"
+            )
+            for br_tag in article_soup.find_all("br"):
+                br_tag.replace_with("\n")
+
+            lines = []
+            for paragraph_tag in article_soup.find_all("p", class_="body-text"):
+                for line in self.linebreak_regex.split(paragraph_tag.get_text()):
+                    lines.append(line.strip())
+            article_loader.add_value("content", "\n".join(lines))
+            yield article_loader.load_item()
